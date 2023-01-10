201bc9f742c327da10f451d42cd8b6f5c530c29d
Titouan-Esc <titouanescorneboueu@gmail.com>
2023-01-10 22:24:51 +0100

    add some features

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module flit_docker
 
 go 1.19
+
+require github.com/lib/pq v1.10.7 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
 
+	_ "github.com/lib/pq"
+)
+
+type User struct {
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func main() {
+	db := DB()
+
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		var user User
+		rows, err := db.Query(`SELECT * FROM "users"`)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+
+		for rows.Next() {
+			if err := rows.Scan(&user.Id, &user.Name, &user.Email, &user.Password); err != nil {
+				log.Fatalln(err.Error())
+			}
+		}
+
+		fmt.Fprintf(res, "%v", user)
+	})
+
+	if err := http.ListenAndServe(":4200", nil); err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func DB() *sql.DB {
+	// 192.168.0.32/16
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable search_path=public", "localhost", "5436", "postgres", "postgres", "golang")
+	conn, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Println(err.Error())
+		return nil
+	}
+	return conn
 }
